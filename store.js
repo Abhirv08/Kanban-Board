bce28a85be6a9ee2854ffcf1de949faca623acca
Abhirv08 <abhishekk.ic.20@nitj.ac.in>
2023-12-23 06:24:42 +0530

    improve README and implemented state remembering functionality

--- store.js
+++ store.js
@@ -1,13 +1,31 @@
 import { create } from 'zustand'
 
-export const useBoundStore = create((set) => ({
-    ordering: "priority",
-    setOrdering: (order) => set((state) => ({ ordering: order })),
-    grouping: "status",
-    setGrouping: (group) => set((state) => ({ grouping: group })),
-    tickets: [],
-    setTickets: (data) => set((state) => ({ tickets: data })),
-    users: [],
-    setUsers: (data) => set((state) => ({ users: data })),
-}))
+export const useBoundStore = create((set) => {
+    const isLocalStorageAvailable = typeof localStorage !== 'undefined';
+
+    // Read initial values from local storage if available, otherwise use defaults
+    const initialGrouping = isLocalStorageAvailable ? localStorage.getItem('grouping') || 'status' : 'status';
+    const initialOrdering = isLocalStorageAvailable ? localStorage.getItem('ordering') || 'priority' : 'priority';
+
+    return {
+        ordering: initialOrdering,
+        setOrdering: (order) => {
+            set(() => {
+                localStorage.setItem('ordering', order);
+                return { ordering: order };
+            });
+        },
+        grouping: initialGrouping,
+        setGrouping: (group) => {
+            set(() => {
+                localStorage.setItem('grouping', group);
+                return { grouping: group };
+            });
+        },
+        tickets: [],
+        setTickets: (data) => set(() => ({ tickets: data })),
+        users: [],
+        setUsers: (data) => set(() => ({ users: data })),
+    };
+});
 
